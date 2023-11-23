70c12d604759c44ff8f7bd70ec95369d77ed7b2e
Springhawk27 <sajjad0mahmud@gmail.com>
2023-11-23 21:44:56 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+// import ExpenseChart from "./ExpenseChart";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Expense Chart</h1>
+    </div>
+  );
+}
+
+export default App;
